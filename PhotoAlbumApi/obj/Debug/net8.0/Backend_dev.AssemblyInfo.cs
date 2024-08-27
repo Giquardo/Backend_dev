692e4a14539bd9a5ec1eaef655124e8d69abd352
giquardo vandaele <giquardo@telenet.be>
2024-08-27 16:33:18 +0200

    working unit tests for everything

--- PhotoAlbumApi/obj/Debug/net8.0/Backend_dev.AssemblyInfo.cs
+++ PhotoAlbumApi/obj/Debug/net8.0/Backend_dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+303eb8369794556e10062acb1ca0255ab6a0dc6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f22e46dbf380d9789dc5b97bc8b7ff985c7edf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

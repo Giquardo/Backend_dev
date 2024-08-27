bb11a36373c18d25afad422bf7dbe3fc4e45d742
giquardo vandaele <giquardo@telenet.be>
2024-08-28 01:13:46 +0200

    docker not working

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120a5b8629548fcb23923125ec978245c32fb000")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9619d8afe97ad96383d036ff8428eb17096524")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

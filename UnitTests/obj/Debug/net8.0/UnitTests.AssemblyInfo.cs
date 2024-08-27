0dfe3bf5bec3fe264d2051a5d62fc5aedef84e49
giquardo vandaele <giquardo@telenet.be>
2024-08-27 20:11:16 +0200

    working grpc

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f22e46dbf380d9789dc5b97bc8b7ff985c7edf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120a5b8629548fcb23923125ec978245c32fb000")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

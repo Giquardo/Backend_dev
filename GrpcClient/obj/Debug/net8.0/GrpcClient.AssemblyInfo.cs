80deb80e346a4dd99b78b35e0e8d1e21d0ce367b
giquardo vandaele <giquardo@telenet.be>
2024-08-28 10:46:12 +0200

    CORS update

--- GrpcClient/obj/Debug/net8.0/GrpcClient.AssemblyInfo.cs
+++ GrpcClient/obj/Debug/net8.0/GrpcClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrpcClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb11a36373c18d25afad422bf7dbe3fc4e45d742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd845babd6cba4bd2cc5a4d4c0dc4f4da71690d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrpcClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrpcClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IntegrationTests/obj/Debug/net8.0/IntegrationTests.AssemblyInfo.cs
+++ IntegrationTests/obj/Debug/net8.0/IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb11a36373c18d25afad422bf7dbe3fc4e45d742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd845babd6cba4bd2cc5a4d4c0dc4f4da71690d")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
